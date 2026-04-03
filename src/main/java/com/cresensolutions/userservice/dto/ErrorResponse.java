84f05c39f26cf64f9bb49a7630b27c5a003ae803
DevanshRajani25 <devanshrajani25@gmail.com>
2026-04-03 19:32:20 +0530

    feat: schema created for Success and Error response for frontend

---
+++ src/main/java/com/cresensolutions/userservice/dto/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.cresensolutions.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorResponse {
+    private String message;
+    private int status;
+}
