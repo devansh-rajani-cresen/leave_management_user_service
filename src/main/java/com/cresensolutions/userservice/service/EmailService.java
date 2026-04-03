f1a39402924559186353ca778713ad6e154a496d
DevanshRajani25 <devanshrajani25@gmail.com>
2026-04-03 19:37:34 +0530

    feat: Email sending part done with use of thymeleaf for better formatting

--- src/main/java/com/cresensolutions/userservice/service/EmailService.java
+++ src/main/java/com/cresensolutions/userservice/service/EmailService.java
@@ -1,23 +1,9 @@
 package com.cresensolutions.userservice.service;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+public interface EmailService {
+    // for sending OTP on forgot password
+    void sendOtp(String toEmail, String otp);
 
-@Service
-public class EmailService {
-
-    private final JavaMailSender mailSender;
-
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    public void sendOtp(String toEmail, String otp){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Password Reset OTP for Cresen Solutions");
-        message.setText("Your OTP for Password Reset is: "+otp+". This will be valid for 5 mins Only.");
-        mailSender.send(message);
-    }
+    // for sending welcoming message on creating new employee
+    void sendWelcomeMessage(String toEmail, String username, String password);
 }
---
+++ src/main/java/com/cresensolutions/userservice/service/EmailServiceImpl.java
@@ -0,0 +1,88 @@
+package com.cresensolutions.userservice.service;
+
+import com.cresensolutions.userservice.exception.CustomException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Slf4j
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    public EmailServiceImpl(JavaMailSender mailSender, TemplateEngine templateEngine) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    // will execute this method to send OTP message during Forgot Password
+    @Override
+    public void sendOtp(String toEmail, String otp) {
+        log.info("Sending OTP to Email: {}", toEmail);
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom(fromEmail, "Cresen Solutions");
+            helper.setTo(toEmail);
+            helper.setSubject("Password Reset OTP for Cresen Solutions");
+
+            // Thymeleaf context
+            Context context = new Context();
+            context.setVariable("otp", otp);
+
+            String htmlContent = templateEngine.process("otp-email", context);
+
+            helper.setText(htmlContent, true);
+            mailSender.send(message);
+
+        } catch (Exception e) {
+            log.error("Error while sending OTP email", e);
+            throw new CustomException(
+                    "Unable to Send OTP. Please try again later.",
+                    500
+            );
+        }
+    }
+
+    // have to send Welcome Email on newly created Employee's Email with Login button
+    @Override
+    public void sendWelcomeMessage(String toEmail, String username, String password) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom(fromEmail, "Cresen Solutions");
+            helper.setTo(toEmail);
+            helper.setSubject("Welcome to Cresen Solutions!");
+
+            Context context = new Context();
+            context.setVariable("username", username);
+            context.setVariable("password", password);
+            context.setVariable("loginUrl", "http://localhost:4200/auth/login");
+
+            String htmlContent = templateEngine.process("welcome-email", context);
+
+            helper.setText(htmlContent, true);
+            mailSender.send(message);
+
+        } catch (Exception e) {
+            log.error("Error while sending Welcome email", e);
+            throw new CustomException(
+                    "Unable to send welcome mail. Please contact support.",
+                    500
+            );
+        }
+    }
+}
