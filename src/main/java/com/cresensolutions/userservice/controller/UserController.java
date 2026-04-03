58fb7f00a8b0352dd3960d1933d1782e21c79549
DevanshRajani25 <devanshrajani25@gmail.com>
2026-04-03 19:16:55 +0530

    fix: seperated admin routes from user routes

--- src/main/java/com/cresensolutions/userservice/controller/UserController.java
+++ src/main/java/com/cresensolutions/userservice/controller/UserController.java
@@ -2,11 +2,11 @@ package com.cresensolutions.userservice.controller;
 
 import com.cresensolutions.userservice.dto.*;
 import com.cresensolutions.userservice.repository.UserRepository;
-import com.cresensolutions.userservice.service.ForgotPasswordService;
-import com.cresensolutions.userservice.service.ResetPassword;
-import com.cresensolutions.userservice.service.UserService;
+import com.cresensolutions.userservice.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,40 +17,33 @@ public class UserController {
     public final UserService userService;
     public final UserRepository userRepository;
     private final ForgotPasswordService forgotPasswordService;
-    private final ResetPassword resetPassword;
+    private final ResetPasswordService resetPasswordService;
 
-    public UserController(UserService userService, UserRepository userRepository, ForgotPasswordService forgotPasswordService, ResetPassword resetPassword) {
+    public UserController(UserService userService, UserRepository userRepository, ForgotPasswordService forgotPasswordService, ResetPasswordService resetPasswordService) {
         this.userService = userService;
         this.userRepository = userRepository;
         this.forgotPasswordService = forgotPasswordService;
-        this.resetPassword = resetPassword;
-    }
-
-    @GetMapping("/test")
-    public String test(){
-        long count = userRepository.count();
-        return "Total users: "+count;
+        this.resetPasswordService = resetPasswordService;
     }
 
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody LoginRequest request){
-        log.info("Request received: {}", request);
+    public LoginResponse login(@RequestBody LoginRequest request) {
         return userService.login(request);
     }
 
     @PostMapping("/send-otp")
-    public boolean sendOtp(@RequestBody SendOtp sendOtpRequest){
+    public boolean sendOtp(@RequestBody SendOtp sendOtpRequest) {
+        log.info("Sending OTP for Email: {}",sendOtpRequest.getEmail());
         return forgotPasswordService.sendOtp(sendOtpRequest);
     }
 
     @PostMapping("/verify-otp")
-    public boolean verifyOtp(@RequestBody VerifyOtp verifyOtpRequest){
+    public boolean verifyOtp(@RequestBody VerifyOtp verifyOtpRequest) {
         return forgotPasswordService.verifyOtp(verifyOtpRequest);
     }
 
     @PostMapping("/reset-password")
-    public boolean resetPassword(@RequestBody ResetPasswordRequest resetPasswordRequest){
-        log.info("Payload from frontend for Reset Password : {}", resetPasswordRequest);
-        return resetPassword.resetPassword(resetPasswordRequest);
+    public boolean resetPassword(@RequestBody ResetPasswordRequest resetPasswordRequest) {
+        return resetPasswordService.resetPassword(resetPasswordRequest);
     }
 }
