536bb6912b3490b72f3b40ca2ac3ecd763ebc0ff
DevanshRajani25 <devanshrajani25@gmail.com>
2026-04-03 19:42:13 +0530

    feat: new functionality added for finding and deleting user by username

--- src/main/java/com/cresensolutions/userservice/repository/UserRepository.java
+++ src/main/java/com/cresensolutions/userservice/repository/UserRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUserName(String userName);
+    boolean existsByUserName(String userName);
+    void deleteByUserName(String userName);
 }
