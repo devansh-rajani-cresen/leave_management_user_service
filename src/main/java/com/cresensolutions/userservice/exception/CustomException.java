745aef2529f7b11d990a5448aa57e3bf5497d53d
DevanshRajani25 <devanshrajani25@gmail.com>
2026-04-03 19:51:59 +0530

    feat: Custom exception handler added for proper response

---
+++ src/main/java/com/cresensolutions/userservice/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.cresensolutions.userservice.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final int status;
+
+    public CustomException(String message, int status) {
+        super(message);
+        this.status = status;
+    }
+}
