f8478bcaad689de6cba29fd560c61927d7f5b4c8
DevanshRajani25 <devanshrajani25@gmail.com>
2026-04-03 19:44:42 +0530

    fix: added custom exception for better error handling

--- src/main/java/com/cresensolutions/userservice/service/UserServiceImpl.java
+++ src/main/java/com/cresensolutions/userservice/service/UserServiceImpl.java
@@ -3,11 +3,14 @@ package com.cresensolutions.userservice.service;
 import com.cresensolutions.userservice.dto.LoginRequest;
 import com.cresensolutions.userservice.dto.LoginResponse;
 import com.cresensolutions.userservice.entity.User;
+import com.cresensolutions.userservice.exception.CustomException;
 import com.cresensolutions.userservice.repository.UserRepository;
 import com.cresensolutions.userservice.util.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 @Service
@@ -27,34 +30,32 @@ public class UserServiceImpl implements UserService {
     @Override
     public LoginResponse login(LoginRequest request) {
 
-        log.info("Request : {}", request);
-
         String username = request.getUsername();
         String password = request.getPassword();
-        log.info("Username: {}", username);
 
         Optional<User> dbUser = userRepository.findByUserName(username);
-        log.info("Fetched user from DB: {}", dbUser);
 
         if (dbUser.isEmpty()) {
-            throw new RuntimeException("User not exists in DB!");
+            throw new CustomException("User does not exist", 404);
         }
 
         // If user founds with the proper credentials (username)
         User user = dbUser.get();
         if (!passwordEncoder.matches(password, user.getUserPswd())) {
-            throw new RuntimeException("Invalid Credentials!");
+            throw new CustomException("Invalid credentials", 401);
         }
 
-        String correct_user_name = user.getUserName();
-        String correct_user_role = user.getRole();
+        String correctUserName = user.getUserName();
+        String correctUserRole = user.getRole();
+        String correctUserFullName = user.getFullName();
 
-        String token = jwtUtil.generateToken(correct_user_name, correct_user_role);
+        String token = jwtUtil.generateToken(correctUserName, correctUserRole);
 
         return new LoginResponse(
                 token,
-                correct_user_role,
-                correct_user_name
+                correctUserRole,
+                correctUserName,
+                correctUserFullName
         );
     }
 }
