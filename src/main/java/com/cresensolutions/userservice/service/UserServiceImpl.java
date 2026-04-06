0751409f6022bbcec3152a66e15c91246d2f1f34
DevanshRajani25 <devanshrajani25@gmail.com>
2026-04-06 16:13:50 +0530

    fix: removed unused transactional

--- src/main/java/com/cresensolutions/userservice/service/UserServiceImpl.java
+++ src/main/java/com/cresensolutions/userservice/service/UserServiceImpl.java
@@ -9,8 +9,6 @@ import com.cresensolutions.userservice.util.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
 
 @Service
