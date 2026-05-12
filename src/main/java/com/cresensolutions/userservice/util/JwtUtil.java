5babb5054da9a2b34572c9014e4007ad1b710f6a
DevanshRajani25 <devanshrajani25@gmail.com>
2026-05-12 09:01:11 +0530

    fix: fixed and enhanced security

--- src/main/java/com/cresensolutions/userservice/util/JwtUtil.java
+++ src/main/java/com/cresensolutions/userservice/util/JwtUtil.java
@@ -3,44 +3,52 @@ package com.cresensolutions.userservice.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String SecretKey = "secretkey@123!";
 
-    public String generateToken(String username, String role){
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username, String role, Long userId, String emailId, String fullName){
         return Jwts.builder()
                 .setSubject(username)
-                .claim("role",role)
+                .claim("role", role)
+                .claim("userId", userId)
+                .claim("emailId", emailId)
+                .claim("fullName", fullName)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SecretKey)
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(SecretKey)
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    // Extracting username
+    public Long extractUserId(String token){
+        Object userId = getClaims(token).get("userId");
+        return userId != null ? Long.parseLong(userId.toString()) : null;
+    }
+
     public String extractUsername(String token){
         return getClaims(token).getSubject();
     }
 
-    // Extracting role
     public String extractRole(String token){
-        return (String) getClaims(token).get("role");
+        return getClaims(token).get("role").toString();
     }
 
-    // Validate token (Expiry check)
     public boolean validateToken(String token){
         return getClaims(token).getExpiration().after(new Date());
     }
-
 }
