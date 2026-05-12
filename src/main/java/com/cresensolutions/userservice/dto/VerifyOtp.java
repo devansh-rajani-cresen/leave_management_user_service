06d89b2dd487abd857195092bd6b60dd6fe1ac04
DevanshRajani25 <devanshrajani25@gmail.com>
2026-05-12 10:29:42 +0530

    fix: @NotBlank added

--- src/main/java/com/cresensolutions/userservice/dto/VerifyOtp.java
+++ src/main/java/com/cresensolutions/userservice/dto/VerifyOtp.java
@@ -1,9 +1,12 @@
 package com.cresensolutions.userservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class VerifyOtp {
+    @NotBlank
     private String email;
+    @NotBlank
     private String otp;
 }
